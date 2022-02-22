909989c31c5b448f50c6155b50487e320286756f
Readpato <87768087+Readpato@users.noreply.github.com>
2022-02-22 10:50:26 +0100

    feat: Declare fetch function
    The function that is going to get our data from the API has been declared and will run after the lifecycle created of our component is achieved

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,43 @@
 <template>
-  <div class="home">Hello World</div>
+  <main v-if="!loading">Show Data</main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+    <img
+      :src="loadingImage"
+      class="w-24 m-auto"
+      alt="Gif of a hourglass that turns."
+    />
+  </main>
 </template>
 
 <script>
 export default {
   name: "HomeView",
   components: {},
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const response = await fetch("https://api.covid19api.com/summary");
+      const data = await response.json();
+      return data;
+    },
+  },
+  async created() {
+    const covidData = await this.fetchCovidData();
+    console.log(covidData);
+    this.dataDate = covidData.Date;
+    this.stats = covidData.Global;
+    this.countries = covidData.Countries;
+    this.loading = false;
+  },
 };
 </script>
